ac27da9c17a398d82059c6e9fdda9aa1d6d1752c
河合桜汰 <o.k.20020929@icloud.com>
2023-11-17 23:20:39 +0900

    200submit

--- test_B/200.py
+++ test_B/200.py
@@ -1,1 +1,10 @@
-s
+n,k = map(int,input().split())
+
+for _ in range(k):
+    if n % 200 == 0:
+        n = n // 200
+    else:
+        n = str(n) + '200'
+        n = int(n)
+
+print(n)            
